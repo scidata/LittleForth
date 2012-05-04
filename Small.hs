834c0f790ce041b5057bbcebcfd1ce9c1eb68021
arapede <jon.templeman@gmail.com>
2012-05-04 21:03:37 +0100

    Now has Forthy definition.

--- Small.hs
+++ Small.hs
@@ -5,35 +5,40 @@ import qualified Data.Map as M
 import Text.ParserCombinators.Parsec
 
 data Word = Word String | Num Int deriving (Ord,Eq)
+data Def = PrimDef (Stack -> Stack) | Def Stack 
 
 instance Show Word where
     show (Num n) = show n
     show (Word s) = s
 
 type Stack = [Word] 
-type Dict = M.Map Word (Stack -> Stack)
-type Env = (Stack,Stack,Dict)
+type Dict = M.Map Word Def
+type Env = (Stack,Dict)
 
 basicDict :: Dict
-basicDict = M.fromList [(Word "+",\ (Num x:Num y:rest) -> (Num (x+y)):rest),
-                        (Word "-",\ (Num x:Num y:rest) -> (Num (x-y)):rest),
-                        (Word "*",\ (Num x:Num y:rest) -> (Num (x*y)):rest),
-                        (Word "drop",\ (_:rest) -> rest),
-                        (Word "dup",\ (x:rest) -> x:x:rest),
-                        (Word "flip",\ (x:y:rest) -> y:x:rest)]
+basicDict = M.fromList [(Word "+",PrimDef $ \ (Num x:Num y:rest) -> (Num (y+x)):rest),
+                        (Word "-",PrimDef $ \ (Num x:Num y:rest) -> (Num (y-x)):rest),
+                        (Word "*",PrimDef $ \ (Num x:Num y:rest) -> (Num (y*x)):rest),
+                        (Word "/",PrimDef $ \ (Num x:Num y:rest) -> (Num (y `div` x)):rest),
+                        (Word "drop",PrimDef $ \ (_:rest) -> rest),
+                        (Word "dup",PrimDef $ \ (x:rest) -> x:x:rest),
+                        (Word "flip",PrimDef $ \ (x:y:rest) -> y:x:rest)]
 
-enter :: [Word] -> Env -> Env 
-enter [] env = env 
-enter i@(w:ws) (d,s,dict) | head (reverse i) == Word "." = eval (d++ws,s,dict)
-                          | otherwise = enter ws (d++[w],s,dict)
+eval :: [Word] -> Env -> Env 
+eval [] env = env
+eval ((Word ":"):ws) env = makeDef ws env 
+eval (w@(Num _):ws) (s,dict) = eval ws (w:s,dict)
+eval (w@(Word _):ws) (s,dict) = case M.lookup w dict of 
+                                    Just fn -> eval ws $ applyDef fn s dict 
+                                    Nothing -> error "Unbound"
 
-eval :: Env -> Env
-eval e@([],s,dict) = e
-eval ((n@(Num _):ws),s,dict) = eval (ws,n:s,dict)
-eval ((w@(Word _):ws),s,dict) = case M.lookup w dict of 
-                                  Just fn -> (ws,fn s,dict) 
-                                -- This needs to recur. 
-                                  Nothing -> error "Unbound"
+makeDef :: [Word] -> Env -> Env 
+makeDef ws (s,dict) = eval rest (s,M.insert name (Def body) dict)
+    where ((name:body),(_:rest)) = break (==(Word ";")) ws
+
+applyDef :: Def -> [Word] -> Dict -> Env
+applyDef (PrimDef fn) args dict = (fn args,dict)
+applyDef (Def fn) args dict = eval fn (args,dict)
 
 readWord :: String -> Word 
 readWord inp = case parse readExpr "Small.hs" inp of Left _ -> error "Parse error"
@@ -45,13 +50,13 @@ readWord inp = case parse readExpr "Small.hs" inp of Left _ -> error "Parse erro
           readExpr = (try readNum) <|> readString 
 
 runInterpreter :: Env -> IO () 
-runInterpreter env@(dStack,sStack,dict) = do 
+runInterpreter env@(stack,dict) = do 
   putStr "> " 
   hFlush stdout
   input <- liftM words getLine 
-  let env'@(d,s,_) = enter (map readWord input) env 
-  putStrLn $ show d ++ show s
+  let env'@(s,_) = eval (map readWord input) env 
+  putStrLn $ show $ reverse s
   runInterpreter env'
 
 main :: IO ()
-main = runInterpreter ([],[],basicDict)
+main = runInterpreter ([],basicDict)
